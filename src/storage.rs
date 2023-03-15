93214a4066f81969ecb487fcea395a106a33c329
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2023-03-15 09:52:23 -0300

    Feat: Add CRC

--- src/storage.rs
+++ src/storage.rs
@@ -1,3 +1,7 @@
+use crc::{Crc, CRC_16_IBM_SDLC};
+
+const X25: Crc<u16> = crc::Crc::<u16>::new(&crc::CRC_16_IBM_SDLC);
+
 pub struct Entry<'a> {
     timestamp: i64,
     key: &'a [u8],
@@ -15,12 +19,20 @@ impl DiskStorage {
         let key_size = entry.key.len();
         let value_size = entry.value.len();
         let mut buf = Vec::with_capacity(24 + key_size + value_size);
+
         buf.extend_from_slice(&entry.timestamp.to_be_bytes());
         buf.extend_from_slice(&key_size.to_be_bytes());
         buf.extend_from_slice(&value_size.to_be_bytes());
         buf.extend_from_slice(&entry.key);
         buf.extend_from_slice(&entry.value);
-        buf
+
+        let mut data: Vec<u8> = Vec::with_capacity(buf.len() + 2);
+
+        let crc = X25.checksum(buf.as_slice()).to_be_bytes();
+        data.extend_from_slice(&crc);
+        data.append(&mut buf);
+
+        data
     }
 }
 
@@ -41,6 +53,7 @@ mod tests {
         assert_eq!(
             serialized,
             vec![
+                0x99, 0xF0, // crc
                 0x00, 0x00, 0x00, 0x00, 0x5f, 0xee, 0x66, 0x00, // 1609459200
                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, // 3
                 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x00, 0x03, // 3
