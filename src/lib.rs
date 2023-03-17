bccd38c8c29769834e0b0ef288f3cdb29f0eef87
Lucas Vargas Noronha <lvargasnoronha@gmail.com>
2023-03-17 14:37:43 -0300

    Feat: Add lib.rs

---
+++ src/lib.rs
@@ -0,0 +1,6 @@
+mod bitcask;
+mod pkg;
+mod storage;
+
+pub use crate::bitcask::Bitcask;
+pub use crate::storage::DiskStorage;
--- src/main.rs
+++ src/main.rs
@@ -1,12 +1,10 @@
-mod bitcask;
-mod pkg;
-mod storage;
-
 use std::str::from_utf8;
 
+use caskdb::{Bitcask, DiskStorage};
+
 fn main() {
-    let storage = Box::new(storage::DiskStorage::new("/tmp/bitcask"));
-    let mut bitcask = bitcask::Bitcask::new(storage);
+    let storage = Box::new(DiskStorage::new("/tmp/bitcask"));
+    let mut bitcask = Bitcask::new(storage);
     bitcask.set(b"hello", b"world");
     println!(
         "hello {}",
